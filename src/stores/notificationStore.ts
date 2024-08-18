8807b79b45a545590a954238d0599e7bd396ee2e
Twaiz <homenkobodya@gmail.com>
2024-08-18 21:06:37 +0300

    notificationStore 2.0

--- src/stores/notificationStore.ts
+++ src/stores/notificationStore.ts
@@ -1,13 +1,19 @@
 import { makeAutoObservable } from "mobx";
 
+type ButtonType = "basic" | "error";
+
 interface INotificationStore {
+  type: ButtonType;
   titleText: string;
   bodyText: string;
+  button: { text: string; onClick: () => void };
 }
 
 class NotificationStore implements INotificationStore {
+  type: ButtonType = "basic";
   titleText = "";
   bodyText = "";
+  button = { text: "", onClick: () => {} };
 
   constructor() {
     makeAutoObservable(this);
