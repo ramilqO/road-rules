9f835b6ca639d2a52d0fbc46c0c2229835506cb3
Twaiz <homenkobodya@gmail.com>
2024-08-19 23:09:51 +0300

    fix(corrected errorText in notificationStore)

--- src/tools/errorHandling.ts
+++ src/tools/errorHandling.ts
@@ -3,7 +3,7 @@ import notificationStore from "../stores/notificationStore";
 
 export default function errorHandling(error: unknown) {
   if (axios.isAxiosError(error)) {
-    notificationStore.setBodyText(error.message);
+    notificationStore.setBodyText(error.response?.data?.message);
   } else {
     notificationStore.setBodyText("Неизвестная ошибка. Попробуйте заново");
   }
