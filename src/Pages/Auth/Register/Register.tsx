6e68c289c81d9aaeeaf3a12afe891ddfeb403046
Twaiz <homenkobodya@gmail.com>
2024-08-24 15:36:23 +0300

    fix(form reset at login, register)

--- src/Pages/Auth/Register/Register.tsx
+++ src/Pages/Auth/Register/Register.tsx
@@ -1,15 +1,14 @@
 import { Form, redirect, useNavigate } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 
-import authStore from "../../../stores/Auth/authStore";
 import registerStore from "../../../stores/Auth/registerStore";
 import style from "./Register.module.scss";
 
-import Loader from "../../../Ui/Loader/Loader";
 import Button from "../../../Ui/Button/Button";
 import Input from "../../../Ui/Input/Input";
 
 import Passwords from "./Passwords/Passwords";
+import authStore from "../../../stores/Auth/authStore";
 
 const Register = observer(() => {
   const navigate = useNavigate();
@@ -20,8 +19,6 @@ const Register = observer(() => {
     registerStore.passwordFieldIsSuccess &&
     registerStore.repeatPasswordFieldIsSuccess &&
     registerStore.departmentFieldIsSuccess;
-
-  if (authStore.isLoading) return <Loader />;
 
   return (
     <div className={style.register}>
@@ -79,6 +76,11 @@ const Register = observer(() => {
           />
         </div>
       </Form>
+      {authStore.isLoading && (
+        <div className={style.container}>
+          <div className={style.loader}></div>
+        </div>
+      )}
     </div>
   );
 });
