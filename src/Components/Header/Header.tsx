b6c9c37caa4f4ef9ff5b53417d22cdcb1444c868
Twaiz <homenkobodya@gmail.com>
2024-09-10 11:24:38 +0300

    add(appearing hiding header)

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,20 +1,40 @@
+import { useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { useNavigate } from "react-router-dom";
 
 import authStore from "@/stores/Auth/authStore";
-
 import style from "./Header.module.scss";
-
 import Button from "@/Ui/Button/Button";
 import ThemeToggle from "@/tools/ThemeToggle/ThemeToggle";
 
 const Header = observer(() => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
   const navigate = useNavigate();
 
-  return (
-    <header className={style.header}>
-      <ThemeToggle />
+  const handleScroll = () => {
+    if (window.scrollY > lastScrollY) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+    setLastScrollY(window.scrollY);
+  };
 
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lastScrollY]);
+
+  return (
+    <header
+      className={`${style.header} ${
+        isVisible ? style.header__visible : style.header__hidden
+      }`}
+    >
+      <ThemeToggle />
       <nav className={style.navigation}>
         <ul className={style.navigation_auth}>
           <li>
